5147e3a498929c4d77e0d4a6ef871c2e8388d5d2
chaselovesgits <c.s.e.milano@gmail.com>
2016-08-18 19:08:13 +0100

    Update test.rb

--- features/step_definitions/test.rb
+++ features/step_definitions/test.rb
@@ -1,1 +1,12 @@
 
+Given(/^I am on the BookingBug Contact Us page$/) do
+  puts 'step 1'
+end
+
+When(/^I navigate to the Contact Us page$/) do
+  puts 'step 2'
+end
+
+Then(/^I should see an accurate post code "([^"]*)"$/) do |arg1|
+  puts 'step 3'
+end
