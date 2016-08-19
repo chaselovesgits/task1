8449c1c1ac82df2d8ca7b6bb79b90025023490a4
chaselovesgits <c.s.e.milano@gmail.com>
2016-08-19 17:32:58 +0100

    Update test.rb

--- features/step_definitions/test.rb
+++ features/step_definitions/test.rb
@@ -1,18 +1,18 @@
-Given(/^I am on the Acumenci Contact Us page$/) do
- visit('http://www.acumenci.com/contact-us/')
-     expect(page).to have_content("Acumen Commercial", wait:30)
+Given(/^I am on the BookingBug Contact Us page$/) do
+ visit('https://www.bookingbug.co.uk/contact')
+     expect(page).to have_content("Customer support enquiries", wait:30)
 end
 
 When(/^I navigate to the Contact Us page$/) do
   puts 'step 2'
 end
 
-Then(/^I should see an accurate post code "(TW9 1HY)"$/) do |arg1|
+Then(/^I should see an accurate post code "(EC1R 5EJ)"$/) do |arg1|
   puts 'step 3'
 end
 When(/^I submit just an email address and some message text$/) do
-  fill_in('your-email', :with => 'cmilano@bookingbug.com')
-  fill_in('your-message', :with => 'blah blah blah')
+  fill_in('Email', :with => 'cmilano@bookingbug.com')
+  fill_in('Notes__c', :with => 'blah blah blah')
 end
 
 When(/^all other fields are blank$/) do
@@ -20,7 +20,7 @@ When(/^all other fields are blank$/) do
 end
 
 Then(/^I should see errors about other required fields$/) do
-  page.should have_content('Validation errors occurred')
+  page.should have_content('This field is required.')
 end
 
 
