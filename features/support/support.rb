9c5eed96855bc4fc9aee837f3ae87f5f2a301882
chaselovesgits <c.s.e.milano@gmail.com>
2016-08-19 11:33:15 +0100

    Update support.rb

--- features/support/support.rb
+++ features/support/support.rb
@@ -2,12 +2,12 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'pry'
 
-Capybara.register_driver :chrome do |app|
- Capybara::Selenium::Driver.new(app, :browser => :chrome)
+Capybara.register_driver :ff do |app|
+ Capybara::Selenium::Driver.new(app, :browser => :ff)
 end
 
 Capybara.configure do |config|
-  config.default_driver = :chrome
+  config.default_driver = :ff
   config.app_host   = 'http://localhost'
 end
 
@@ -15,6 +15,6 @@ World(Capybara)
 
 
  Before('@e2e_coi @e2e_nex') do
-  ::Capybara.default_driver = :chrome
+  ::Capybara.default_driver = :ff
 ::Capybara.default_wait_time = 90  
 end
